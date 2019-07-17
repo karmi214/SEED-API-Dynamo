315205f45194c3b4d1ed21bbe5995faf93968065
karmi214 <karmi214@gmail.com>
2019-07-17 14:54:11 +0545

    first commit

---
+++ todos/create.py
@@ -0,0 +1,39 @@
+import json
+import logging
+import os
+import time
+import uuid
+from datetime import datetime
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+def create(event, context):
+    data = json.loads(event['body'])
+    if 'text' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the todo item.")
+    
+    timestamp = str(datetime.utcnow().timestamp())
+
+
+
+    item = {
+        'id': str(uuid.uuid1()),
+        'text': data['text'],
+        'checked': False,
+        'createdAt': timestamp,
+        'updatedAt': timestamp,
+    }
+
+    # write the todo to the database
+    table.put_item(Item=item)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(item)
+    }
+
+    return response
