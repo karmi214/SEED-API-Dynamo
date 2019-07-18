7ab96310c033c73be24484b88a1c7d528e84fd6b
karmi214 <karmi214@gmail.com>
2019-07-18 11:48:12 +0545

    Update API working

--- todos/update.py
+++ todos/update.py
@@ -8,37 +8,36 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 def update(event, context):
     data = json.loads(event['body'])
-    if 'id' not in data:
+    # data=event['body']
+    if 'id' not in event['pathParameters']:
         logging.error("Validation Failed")
         raise Exception("Couldn't update the todo item.")
         return
-
-
-
+    
     # update the todo in the database
-    # result = table.update_item(
-    #     Key={
-    #         'id': event['pathParameters']['id']
-    #     },
-    #     ExpressionAttributeNames={
-    #       '#todo_id': 'id',
-    #     },
-    #     ExpressionAttributeValues={
-    #       ':id': data['id'],
-    #       ':name': data['name'],
-    #       ':email': data['email'],
-    #     },
-    #     UpdateExpression='SET #todo_id = :id, '
-    #                      'name = :name, '
-    #                      'email = :email, ',
-    #     ReturnValues='ALL_NEW',
-    # )
+    result=table.update_item(
+            Key={
+                'id': event['pathParameters']['id']
+            },
+            AttributeUpdates={
+                'name':{
+                    'Value':data['name'],
+                    'Action':'PUT'
+                },
+                'email':{
+                    'Value':data['email'],
+                    'Action':'PUT'
+                }
+            },
+            ReturnValues='ALL_NEW'
+            
+        )
 
     # create a response
     response = {
         "statusCode": 200,
-        # "body": json.dumps(result['Attributes'])
-        "body":json.dumps("Update called for "+ event['pathParameters']['id'])
+        "body": json.dumps(result['Attributes'])
+        # "body":json.dumps("Update called for "+ event['pathParameters']['id'])
     }
 
     return response
